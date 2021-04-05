6c62f1b93acc2243836653d28418e20cf41f6525
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-04-05 21:21:30 +0530

    Adding test cases for inferBreach function

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,5 +4,26 @@
 #include "typewise-alert.h"
 
 TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  
+  int const NoofTestCases = 6;
+	double test_InputMatrix[NoofTestCases][3] = { {0,0,0},  
+	                                             {12,10,30},//NORMAL
+									 	                           {9,10,30},//TOO_LOW
+									 	                           {35,10,30},//TOO_HIGH
+	                                             {10,10,30}, //BOUNDARY_CONDITION
+									 	                           {30,10,30 }, //BOUNDARY_CONDITION
+	                                           };
+
+	double test_OutputMatrix[NoofTestCases] = { { NORMAL  },
+												                      { NORMAL  },
+												                      { TOO_LOW },
+												                      { TOO_HIGH},
+												                      { NORMAL  },
+												                      { NORMAL  },
+	                                         };
+  
+	for (int TestCaseNr = 0; TestCaseNr < NoofTestCases; TestCaseNr++){
+	
+  REQUIRE((inferBreach(test_InputMatrix[TestCaseNr][0], test_InputMatrix[TestCaseNr][1],
+			                 test_InputMatrix[TestCaseNr][2]) == test_OutputMatrix[TestCaseNr]));
 }
