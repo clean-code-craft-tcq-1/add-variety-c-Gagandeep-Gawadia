0815953b108be6fca93d1a320d5da8589d92ae6c
Gagandeep-Gawadia <77660067+Gagandeep-Gawadia@users.noreply.github.com>
2021-04-05 21:24:37 +0530

    removing build errors

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,24 +6,25 @@
 TEST_CASE("infers the breach according to limits") {
   
   int const NoofTestCases = 6;
-	double test_InputMatrix[NoofTestCases][3] = { {0,0,0},  
-	                                             {12,10,30},//NORMAL
-									 	                           {9,10,30},//TOO_LOW
-									 	                           {35,10,30},//TOO_HIGH
-	                                             {10,10,30}, //BOUNDARY_CONDITION
-									 	                           {30,10,30 }, //BOUNDARY_CONDITION
+  double test_InputMatrix[NoofTestCases][3] = { {0,0,0},  
+	                                        {12,10,30},//NORMAL
+						{9,10,30},//TOO_LOW
+						{35,10,30},//TOO_HIGH
+	                                        {10,10,30}, //BOUNDARY_CONDITION
+						{30,10,30 }, //BOUNDARY_CONDITION
 	                                           };
 
-	double test_OutputMatrix[NoofTestCases] = { { NORMAL  },
-												                      { NORMAL  },
-												                      { TOO_LOW },
-												                      { TOO_HIGH},
-												                      { NORMAL  },
-												                      { NORMAL  },
-	                                         };
+ double test_OutputMatrix[NoofTestCases] = { { NORMAL  },
+					     { NORMAL  },
+					     { TOO_LOW },
+					     { TOO_HIGH},
+					     { NORMAL  },
+					     { NORMAL  },
+	                                   };
   
-	for (int TestCaseNr = 0; TestCaseNr < NoofTestCases; TestCaseNr++){
+  for (int TestCaseNr = 0; TestCaseNr < NoofTestCases; TestCaseNr++){
 	
   REQUIRE((inferBreach(test_InputMatrix[TestCaseNr][0], test_InputMatrix[TestCaseNr][1],
 			                 test_InputMatrix[TestCaseNr][2]) == test_OutputMatrix[TestCaseNr]));
+  }
 }
